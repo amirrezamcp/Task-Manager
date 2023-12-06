90864f90a8f74e7342b99d9d9c3cf2afc0e4572f
bigpotato-yt <138766025+bigpotato-yt@users.noreply.github.com>
2023-12-06 07:47:42 +0330

    query getFolders

--- bootstrap/config.php
+++ bootstrap/config.php
@@ -1,10 +1,14 @@
 <?
 
 $databases_config = (object) [
-    'host' => 'localhost', // Name Host
-    'user' => 'root', // User Name
-    'pass' => '', // Password
-    'db' => 'todolist' // Database Name
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => '',
+    'db' => 'todolist'
 ];
 
+ // Name Host
+ // User Name
+ // Password
+ // Database Name
 ?>
--- libs/lib-task.php
+++ libs/lib-task.php
@@ -1,5 +1,15 @@
 <?php
-function getTask() {
+function getFolders() {
+    global $conn;
+    $sql = "SELECT * FROM `folders`";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $records = $stmt->fetchAll(PDO::FETCH_OBJ);
+    return $records;
+}
+
+#tasks
+function getTasks() {
     return 1 ;
 }
 
