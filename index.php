860f1eda46efdedb661173d3b607afdd93fc22a3
bigpotato-yt <138766025+bigpotato-yt@users.noreply.github.com>
2023-12-08 22:48:19 +0330

    Get & Delete Tasks and Update UI

--- index.php
+++ index.php
@@ -5,9 +5,10 @@ include_once ("./bootstrap/init.php");
 if (isset($_GET['delete_folder']) && is_numeric($_GET['delete_folder'])) {
    $deleteedCount = deleteFolder($_GET['delete_folder']);
 }
-
+if (isset($_GET['delete_task']) && is_numeric($_GET['delete_task'])) {
+   $deleteedCount = deleteTask($_GET['delete_task']);
+}
 $folders = getFolders();
 $tasks = getTasks();
-
 include_once ("./tpl/trpl-todo-list.php");
 ?>
