4c7233c669893238fe7b879e05bb845ffbc10f13
bigpotato-yt <138766025+bigpotato-yt@users.noreply.github.com>
2023-12-16 07:34:51 +0330

    Add file authentication

--- tpl/tpl-auth.php
+++ tpl/tpl-auth.php
@@ -1,20 +1,42 @@
-
 <!DOCTYPE html>
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title> Login/Reisters </title>
-  <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/style.css">
+  <title>CodePen - login-signup</title>
+  <link rel="stylesheet" href="./style.css">
 
 </head>
 <body>
-<!-- partial:index.partial.html -->a
-<div class="page">
-    Auth Forms Here ...
+<!-- partial:index.partial.html -->
+<div id="background">
+	<div id="panel-box">
+		<div class="panel">
+			<div class="auth-form on" id="login">
+				<div id="form-title">Log In</div>
+				<form action="/login" method="POST">
+					<input name="Username" type="text" required="required" placeholder="username"/>
+					<input name="Password" type="password" required="required" placeholder="password"/>
+					<button type="Submit">Log In</button>
+				</form>
+			</div>
+			<div class="auth-form" id="signup" >
+				<div id="form-title">Register</div>
+				<form action="/login" method="POST">
+					<input name="Username" type="text" required="required" placeholder="username"/>
+					<input name="Password" type="password" required="required" placeholder="password"/>
+					<button type="Submit">Sign Up</button>
+				</form>
+			</div>
+		</div>
+		<div class="panel">
+			<div id="switch">Sign Up</div>
+			<div id="image-overlay"></div>
+			<div id="image-side"></div>
+		</div>
+	</div>
 </div>
 <!-- partial -->
-  <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-  <script  src="<?= BASE_URL ?>assets/js/script.js"></script>
+  <script src='https://code.jquery.com/jquery-3.3.1.min.js'></script><script  src="./script.js"></script>
 
 </body>
 </html>
